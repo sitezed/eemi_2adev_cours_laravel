188b70d549ee4bc7ba6b57a3dcc550e1d7b37e92
ziad <ziad.jb@gmail.com>
2018-02-07 12:55:05 +0100

    ok

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -2,12 +2,18 @@
 
 namespace Eemi\Http\Controllers;
 
+use Eemi\Produit;
 use Illuminate\Http\Request;
 
 class ProduitController extends Controller
 {
 	/* cette methode est appelée dans routes/web.php via l'url /liste_produits */
     public function listeProduits(){
-    	return view('liste_produits');
+
+    	// je recupere tous les produits
+    	$data['produits'] = Produit::all();
+
+    	// je les envois à la vue
+    	return view('liste_produits', $data);
     }
 }
--- resources/views/liste_produits.blade.php
+++ resources/views/liste_produits.blade.php
@@ -5,6 +5,18 @@
 @section('contenu')
 
   <h1>Liste des produits</h1>
-  <div class="alert alert-danger">Test</div>
+
+  @foreach($produits as $produit)
+    {{ $produit->id }}<br>
+    {{ $produit->titre }}<br>
+    {{ $produit->reference }}<br>
+    {{ $produit->slug }}<br>
+    {{ $produit->photo }}<br>
+    {{ $produit->prix }}<br>
+    {{ $produit->quantite }}<br>
+    {{ $produit->description }}<br>
+    {{ $produit->created_at }}<br>
+    {{ $produit->updated_at }}<br>
+  @endforeach
 
 @endsection
